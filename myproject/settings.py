ed283a6a50c371b3c4f019c4cd1a3428ac41ea01
dapsfromends <danieloladapo78@outlook.com>
2025-03-08 22:52:42 +0000

    Updated dependencies

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-8r-cc17l7#g@vmid91w=cu%()@5&(osj3=kypu()f_^z8m!j*_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
