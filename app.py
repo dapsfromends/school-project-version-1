0d2ae93f7a0488d6fa802417dc113cfef51ece01
dapsfromends <danieloladapo78@outlook.com>
2025-03-09 23:33:55 +0000

    new update3

--- app.py
+++ app.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return "<h2>About Page</h2><p>This is a simple web app built with Flask.</p>"
-
 if __name__ == "__main__":
     app.run(debug=True)
